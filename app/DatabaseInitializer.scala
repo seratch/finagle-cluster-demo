e9898d62b542e8717deaa73b13818f1597d39b2d
Kazuhiro Sera <seratch@gmail.com>
2012-05-25 20:03:07 +0900

    Refactored at #miyamathon

--- app/DatabaseInitializer.scala
+++ app/DatabaseInitializer.scala
@@ -1,25 +1,23 @@
 import play.api._
-
 import scalikejdbc._
 
-object Global extends GlobalSettings {
+class DatabaseInitializer(app: Application) extends Plugin {
 
-  Class.forName("org.hsqldb.jdbc.JDBCDriver")
-  ConnectionPool.singleton("jdbc:hsqldb:mem:finagle_hack","sa","")
+  DB autoCommit {
+    implicit session =>
 
-  DB autoCommit { implicit session =>
-
-    try {
-      SQL("SELECT COUNT(1) FROM MESSAGE").map(rs => rs.long(1)).single.apply()
-    } catch { case e =>
-      SQL("""
+      try {
+        SQL("SELECT COUNT(1) FROM MESSAGE").map(rs => rs.long(1)).single.apply()
+      } catch {
+        case e =>
+          SQL( """
       CREATE TABLE MESSAGE (
         ID BIGINT PRIMARY KEY NOT NULL,
         SERVER_NAME VARCHAR(16),
         CURRENT_STATUS VARCHAR(16) NOT NULL
       )
-          """).execute.apply()
-      SQL("""
+               """).execute.apply()
+          SQL( """
       CREATE TABLE MESSAGE_HISTORY (
         ID BIGINT GENERATED ALWAYS AS IDENTITY,
         MESSAGE_ID BIGINT NOT NULL,
@@ -27,8 +25,8 @@ object Global extends GlobalSettings {
         SERVER_NAME VARCHAR(16),
         ERROR_MESSAGE VARCHAR(256)
       );
-          """).execute.apply()
-    }
+               """).execute.apply()
+      }
 
   }
 
